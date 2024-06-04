7bcd86db6215ae2b6ef4560f34c5e64507c565d5
Cfy1010 <cy_one2000@yahoo.fr>
2024-06-04 17:30:17 +0200

    suite structure forme & add newrecord

--- client/src/pages/dashboard/financial-record-form.tsx
+++ client/src/pages/dashboard/financial-record-form.tsx
@@ -1,18 +1,47 @@
+import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+
 export const FinancialRecordForm = () => {
+  const [description, setDescription] = useState<string>("");
+  const [amount, setAmount] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [payementMethod, setPayementMethod] = useState<string>("");
+
+  const user = useUser();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newRecord = {
+      userId: user?.id,
+      date: new Date(),
+      description: description,
+      amount: parseFloat(amount),
+      category: category,
+      payementMethod: payementMethod,
+    };
+
+    // addRecord(newRecord)
+    setDescription("");
+    setAmount("");
+    setCategory("");
+    setPayementMethod("");
+  };
+
   return (
     <div className="form-container">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label htmlFor="">Description : </label>
-          <input type="text" className="input" required />
+          <input type="text" className="input" required value={description} />
         </div>
         <div className="form-field">
           <label htmlFor="">Montant : </label>
-          <input type="number" className="input" required />
+          <input type="number" className="input" required value={amount} />
         </div>
         <div className="form-field">
           <label htmlFor="">Categorie : </label>
-          <select className="input" name="" id="" required>
+          <select className="input" required value={category}>
             <option value="Food">Nourriture</option>
             <option value="Rent">Rent</option>
             <option value="Salary">Salaire</option>
@@ -21,7 +50,18 @@ export const FinancialRecordForm = () => {
             <option value="Other">Other</option>
           </select>
         </div>
-        <button>Validation</button>
+        <div className="form-field">
+          <label htmlFor="">Methode de Payement : </label>
+          <select className="input" required value={payementMethod}>
+            <option value="Food">Selectionne une methode de paiement</option>
+            <option value="Rent">Carte de Credit</option>
+            <option value="Salary">Cash</option>
+            <option value="Utilities">Virement</option>
+          </select>
+        </div>
+        <button type="submit" className="button">
+          Ajoute un enregistrement
+        </button>
       </form>
     </div>
   );
